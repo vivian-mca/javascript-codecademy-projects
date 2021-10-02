ed2ed6f4a1d751a7597dd4647dfc06c8652eccb3
Vivian A <73986618+vivian-mca@users.noreply.github.com>
2021-10-02 10:37:59 -0500

    Update app.js

--- Kelvin Weather/app.js
+++ Kelvin Weather/app.js
@@ -1,16 +1,17 @@
-// Current temperature in Kelvin.
-const kelvin = 0;
-// Convert Kelvin to Celsius.
+// Current temperature forecast in Kelvin
+const kelvin = 293;
+
+// Converts Kelvin to Celsius
 const celsius = kelvin - 273;
-// Convert Celsius to Fahrenheit.
-let fahrenheit = celsius * (9/5) + 32;
-// Round down Fahrenheit variable.
-fahrenheit = Math.floor(fahrenheit);
 
-console.log(`The temperature is ${fahrenheit} degrees of Fahrenheit.`);
+// Converts Celsius to Fahrenheit and rounds down the result
+const fahrenheit = Math.floor(celsius * (9/5) + 32);
 
-// Convert Celsius to Newton
-let newton = celsius * (33/100);
-newton = Math.floor(newton);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
-console.log(`The temperature is ${newton} Newton.`);
+// --EXTRA PRACTICE--
+
+const newton = Math.floor(celsius * (33/100));
+console.log(`The temperature is ${newton} degrees Newton.`);
+
+
