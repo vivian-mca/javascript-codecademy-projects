d79c6cbfe80356f5a555886c53a59fbb104b4de0
Vivian A <73986618+vivian-mca@users.noreply.github.com>
2021-10-03 15:24:29 -0500

    Update main.js

--- Magic Eight Ball/main.js
+++ Magic Eight Ball/main.js
@@ -1,4 +1,3 @@
-// Input username option
 let userName = '';
 userName ? console.log(`Hello, ${userName}`) : console.log('Hello!');
 
@@ -9,7 +8,7 @@ console.log(`The user asked: ${userQuestion}`);
 // Generates a random number between 0 and 7
 let randomNumber = Math.floor(Math.random() * 8);
 
-// Magic Eight Ball statements based on generated number from randomNumber
+// Magic Eight Ball responses based on generated number
 let eightBall = '';
 switch (randomNumber) {
   case 0:
@@ -38,28 +37,7 @@ switch (randomNumber) {
     break;
   default:
     eightBall = 'Invalid';
-    break;
-};
-
-/* Alternate solution using if..else statements for Magic Eight Ball statements based on generated number from randomNumber
-if (randomNumber = 0) {
-  console.log('It is certain');
-} else if (randomNumber = 1) {
-  console.log ('It is decidedly so');
-} else if (randomNumber = 2) {
-  console.log ('Reply hazy, try again');
-} else if (randomNumber = 3) {
-  console.log ('Cannot predict now');
-} else if (randomNumber = 4) {
-  console.log ('Do not count on it');
-} else if (randomNumber = 5) {
-  console.log ('My sources say no');
-} else if (randomNumber = 6) {
-  console.log ('Outlook not so good');
-} else {
-  console.log ('Signs point to yes')
 }
-*/
 
 // Magic Eight Ball's response
 console.log(`The Magic Eight Ball answered: ${eightBall}`);
